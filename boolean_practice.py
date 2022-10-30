ac17c154df18a7ca9d06577265b2c82567a436ba
Anurag Suraj Bhardwaj <105678749+BhardwajAnurag@users.noreply.github.com>
2022-10-30 14:15:35 -0400

    Create boolean_practice.py

---
+++ boolean_practice.py
@@ -0,0 +1,20 @@
+True and True #True
+False and True #False
+1 == 1 and 2 == 1 #False
+"love" == "love" #T
+1 == 1 or 2 != 1 #T
+True and 1 == 1 #T
+False and 0 != 0 #F
+True or 1 == 1 #T
+"time" == "money" #F
+1 != 0 and 2 == 1 #F
+"I Can't Believe It's Not Butter!" != "butter" #T
+"one" == 1 #F
+not (True and False) #T
+not (1 == 1 and 0 != 1) #F
+not (10 == 1 or 1000 == 1000) #F
+not (1 != 10 or 3 == 4) #T
+not ("love" == "love" and "time" == "money") #T
+1 == 1 and (not ("one" == 1 or 1 == 0)) #T
+"chunky" == "bacon" and (not (3 == 4 or 3 == 3)) #F
+3 == 3 and (not ("love" == "love" or "Python" == "Fun")) #F
